f9b5d5082432f521237c0def8732f9266e88a578
Som <aishwarysrivastava2006@gmail.com>
2023-09-23 13:44:52 +0530

    asda

--- pages/clubs/science/index.js
+++ pages/clubs/science/index.js
@@ -1,21 +1,18 @@
 import Link from "next/link";
 import { useEffect, useState } from "react";
-import {fetchData} from "../../../components/fetchData"
+import { fetchData } from "../../../components/fetchData";
 
 export default function Home() {
-  const [data, setData] = useState([])
+  const [data, setData] = useState([]);
   useEffect(() => {
-    Promise.all([
-      fetchData("clubs"),
-    ]).then(([data]) => {
-      setData(data);
-    }).catch(error => {
-      console.error("Error fetching data:", error);
-    });
-    
-  
-
-  }, [])
+    Promise.all([fetchData("clubs")])
+      .then(([data]) => {
+        setData(data);
+      })
+      .catch((error) => {
+        console.error("Error fetching data:", error);
+      });
+  }, []);
   return (
     <div className="flex   flex-col w-full space-y-3 h-full justify-center p-7 text-white">
       <div className="flex flex-col space-y-2">
