c80c2ea2ca0794f8916622230811d61d96c97492
Som <aishwarysrivastava2006@gmail.com>
2023-09-23 13:41:49 +0530

    asdas

--- pages/events.js
+++ pages/events.js
@@ -1,57 +1,70 @@
 import Link from "next/link";
 import { useEffect, useState } from "react";
-import {fetchData} from "../components/fetchData"
+import { fetchData } from "../components/fetchData";
 
-export default function events({setLoading}) {
-  
-   const [wdata, setWdata] = useState([{}])
-const [cdata, setCdata] = useState([{}])
-const [widata, setWidata] = useState([{}])
-useEffect(() => {
-  setLoading(true)
-  Promise.all([
-    fetchData("workshops"),
-    fetchData("competetions"),
-    fetchData("webinars"),
-  ]).then(([wdata, cdata, widata]) => {
-    setWdata(wdata);
-    setCdata(cdata);
-    setWidata(widata);
-  }).catch(error => {
-    console.error("Error fetching data:", error);
-  });
-    setLoading(false)
-}, []);
+export default function events({ setLoading }) {
+  const [wdata, setWdata] = useState([{}]);
+  const [cdata, setCdata] = useState([{}]);
+  const [widata, setWidata] = useState([{}]);
+  useEffect(() => {
+    setLoading(true);
+    Promise.all([
+      fetchData("workshops"),
+      fetchData("competetions"),
+      fetchData("webinars"),
+    ])
+      .then(([wdata, cdata, widata]) => {
+        setWdata(wdata);
+        setCdata(cdata);
+        setWidata(widata);
+      })
+      .catch((error) => {
+        console.error("Error fetching data:", error);
+      });
+    setLoading(false);
+  }, []);
   function formatDate(inputDate) {
     if (!inputDate) {
-      return 'Invalid date format';
+      return "Invalid date format";
     }
-  // Split the input date using slashes and convert it to an array [day, month, year]
-  const dateParts = inputDate.split('/').map(part => parseInt(part, 10));
+    const dateParts = inputDate.split("/").map((part) => parseInt(part, 10));
 
-  if (dateParts.length !== 3) {
-    return 'Invalid date format'; // Handle invalid input format
+    if (dateParts.length !== 3) {
+      return "Invalid date format";
+    }
+
+    const [day, month, year] = dateParts;
+
+    const monthNames = [
+      "Jan",
+      "Feb",
+      "Mar",
+      "Apr",
+      "May",
+      "Jun",
+      "Jul",
+      "Aug",
+      "Sep",
+      "Oct",
+      "Nov",
+      "Dec",
+    ];
+
+    // Create an array of suffixes for days (st, nd, rd, th)
+    const daySuffix =
+      day === 1 || day === 21 || day === 31
+        ? "st"
+        : day === 2 || day === 22
+        ? "nd"
+        : day === 3 || day === 23
+        ? "rd"
+        : "th";
+
+    // Format the date string
+    const formattedDate = `${day}${daySuffix} ${monthNames[month - 1]} ${year}`;
+
+    return formattedDate;
   }
-
-  // Extract day, month, and year from the array
-  const [day, month, year] = dateParts;
-
-  // Define an array of month names for formatting
-  const monthNames = [
-    'Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun',
-    'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'
-  ];
-
-  // Create an array of suffixes for days (st, nd, rd, th)
-  const daySuffix = (day === 1 || day === 21 || day === 31) ? 'st' :
-                    (day === 2 || day === 22) ? 'nd' :
-                    (day === 3 || day === 23) ? 'rd' : 'th';
-
-  // Format the date string
-  const formattedDate = `${day}${daySuffix} ${monthNames[month - 1]} ${year}`;
-
-  return formattedDate;
-}
   return (
     <div className="flex   flex-col w-full space-y-3 h-full justify-center p-7 text-white">
       <div className="flex flex-col space-y-2">
@@ -77,12 +90,16 @@ useEffect(() => {
                 <div className="flex flex-col  space-y-1">
                   <h1 className="text-xl">Date - {formatDate(e.date)}</h1>
                   <h1 className="text-xl">Location - {e.mode}</h1>
-                  <h1 className="text-xl">Members -  {e.members ? e.members.length : 0}</h1>
+                  <h1 className="text-xl">
+                    Members - {e.members ? e.members.length : 0}
+                  </h1>
                 </div>
                 <div className="flex flex-col space-y-1">
                   <button className="px-12 z-50 cursor-pointer text-xl py-2 rounded-xl bg-[#294D69] text-white justify-center items-center">
                     <Link
-                     href={`/events/competetions/${e.title && e.title.replace(/\s/g, "").toLowerCase()}`}
+                      href={`/events/competetions/${
+                        e.title && e.title.replace(/\s/g, "").toLowerCase()
+                      }`}
                     >
                       Explore
                     </Link>
@@ -112,17 +129,23 @@ useEffect(() => {
                 className="flex min-w-[366px] h-[228px]  py-6  px-6 rounded-xl justify-between items-start flex-col space-y-5"
               >
                 <div className="flex">
-                  <h1 className="text-2xl ">{e.title? e.title: "Untitled"}</h1>
+                  <h1 className="text-2xl ">
+                    {e.title ? e.title : "Untitled"}
+                  </h1>
                 </div>
                 <div className="flex flex-col  space-y-1">
                   <h1 className="text-xl">Date - {formatDate(e.date)}</h1>
                   <h1 className="text-xl">Location - {e.mode}</h1>
-                  <h1 className="text-xl">Participants - {e.members ? e.members.length : 0}</h1>
+                  <h1 className="text-xl">
+                    Participants - {e.members ? e.members.length : 0}
+                  </h1>
                 </div>
                 <div className="flex flex-col space-y-1">
                   <button className="px-12 z-50 cursor-pointer text-xl py-2 rounded-xl bg-[#294D69] text-white justify-center items-center">
                     <Link
-                      href={`/events/workshops/${e.title && e.title.replace(/\s/g, "").toLowerCase()}`}
+                      href={`/events/workshops/${
+                        e.title && e.title.replace(/\s/g, "").toLowerCase()
+                      }`}
                     >
                       Explore
                     </Link>
@@ -152,17 +175,23 @@ useEffect(() => {
                 className="flex min-w-[366px] h-[228px]  py-6  px-6 rounded-xl justify-between items-start flex-col space-y-5"
               >
                 <div className="flex">
-                  <h1 className="text-2xl ">{e.title? e.title: "Untitled"}</h1>
+                  <h1 className="text-2xl ">
+                    {e.title ? e.title : "Untitled"}
+                  </h1>
                 </div>
                 <div className="flex flex-col  space-y-1">
                   <h1 className="text-xl">Date - {formatDate(e.date)}</h1>
                   <h1 className="text-xl">Location - {e.mode}</h1>
-                  <h1 className="text-xl">Members - {e.members ? e.members.length : 0}</h1>
+                  <h1 className="text-xl">
+                    Members - {e.members ? e.members.length : 0}
+                  </h1>
                 </div>
                 <div className="flex flex-col space-y-1">
                   <button className="px-12 z-50 cursor-pointer text-xl py-2 rounded-xl bg-[#294D69] text-white justify-center items-center">
                     <Link
-                      href={`/events/webinars/${e.title && e.title.replace(/\s/g, "").toLowerCase()}`}
+                      href={`/events/webinars/${
+                        e.title && e.title.replace(/\s/g, "").toLowerCase()
+                      }`}
                     >
                       Explore
                     </Link>
